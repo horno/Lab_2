8071bdf2ec0bfeed4d885fb5d6e431c46481f797
horno <44303876+horno@users.noreply.github.com>
2018-11-25 12:46:33 +0100

    Delete HumanComparator.java

--- Lab2_Joaquim_Picó_Ian_Palacín/Hierarchy/HumanComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class HumanComparator implements Comparator<Human> {
-
-    @Override
-    public int compare(Human h1,Human h2){
-        return (Integer.toString(h1.powerLevel)).compareTo(Integer.toString(h2.powerLevel));
-    }
-}
