7c99ffe58e18fc9b959b5f7c7b9482413d7033d7
horno <44303876+horno@users.noreply.github.com>
2018-11-25 12:47:38 +0100

    Delete Mage.java

--- Lab2_Joaquim_Picó_Ian_Palacín/Hierarchy/Mage.java
+++
@@ -1,8 +0,0 @@
-public class Mage extends MagicHuman{
-
-    public Mage(String name){
-        super(name);
-        powerLevel = 25;            //Now the magic human is a mage, so the power level increases to 25;
-    }
-
-}
