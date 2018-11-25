930f2a8890a6a9df9c57764af2275cfc1ebcd0ec
horno <44303876+horno@users.noreply.github.com>
2018-11-25 12:48:04 +0100

    Delete Warrior.java

--- Lab2_Joaquim_Picó_Ian_Palacín/Hierarchy/Warrior.java
+++
@@ -1,8 +0,0 @@
-public class Warrior extends StrongHuman{
-
-    public Warrior(String name){
-        super(name);
-        powerLevel = 20;            //Now the strong human is a warrior, so the power level increases to 20;
-    }
-
-}
